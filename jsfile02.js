df3554130e5bc0b9be99002b21125525e62d028d
ashutosh <ashutoshashu2001@gmail.com>
2024-05-21 09:35:08 +0530

    connversion of datatypes

---
+++ jsfile02.js
@@ -0,0 +1,13 @@
+let score="33"
+console.log(typeof (score))
+//conversion
+let conNum=Number(score)
+console.log(typeof conNum)
+console.log(conNum)
+
+//"33"=>33
+//"33abc"=>NaN
+
+//if we convert empty string into boolean it gives  false 
+//else it gives  true
+
