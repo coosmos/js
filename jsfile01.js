92edb8f67cb9cf76e1a27f1f2733d87342c42df0
ashutosh <ashutoshashu2001@gmail.com>
2024-05-21 09:25:23 +0530

    datatypes

---
+++ jsfile01.js
@@ -0,0 +1,12 @@
+//data variables --prefer  not  to use var
+//beacause of scope 
+
+let name="hitesh"
+let age=28
+let isLoggedIn=false
+// let user={name:"hitesh",age:28} //object
+//null=>standalone value
+//undefined=>variable is declared but not assigned any value
+//symbol =>unqiueness ke liye
+//object
+console.log(typeof null)
