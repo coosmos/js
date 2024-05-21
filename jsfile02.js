3a28354f7e50b3d053f91c30ca0c922ac6823c23
ashutosh <ashutoshashu2001@gmail.com>
2024-05-21 10:06:46 +0530

    operations

--- jsfile02.js
+++ jsfile02.js
@@ -1,13 +1,26 @@
-let score="33"
-console.log(typeof (score))
-//conversion
-let conNum=Number(score)
-console.log(typeof conNum)
-console.log(conNum)
+// let score="33"
+// console.log(typeof (score))
+// //conversion
+// let conNum=Number(score)
+// console.log(typeof conNum)
+// console.log(conNum)
 
 //"33"=>33
 //"33abc"=>NaN
 
 //if we convert empty string into boolean it gives  false 
 //else it gives  true
+/******* operationss      ************** */
+let value=3
+let negvalue=-value
+// console.log(negvalue)
+// console.log(2-2)
+// console.log(2**2)  =>raises 2 to the power of 2 
 
+let str1="hello"
+let str2="ashutosh"
+let str3=str1+str2
+// console.log("1"+2)//prints 12  --standard 
+// console.log("1"+2+3) //converts  the entire exp  to first element datatype and prints 123 
+// console.log(3+2+"1") //prints 51  adds  the first  two numbers and the appends the next number
+let num1,num2,num3
