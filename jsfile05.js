4a627b3a7956aef9835846c20c69c0b2a99ac4a0
ashutosh <ashutoshashu2001@gmail.com>
2024-05-24 21:53:25 +0530

    strings

---
+++ jsfile05.js
@@ -0,0 +1,46 @@
+// let name="ashutosh"
+// let  sirname="singh"
+// let age =12
+// console.log(`hello my name is ${name} and I am ${age} years old`)
+const gamename= new String(`Ashutosh-singh`)
+// // console.log(gamename[0])
+// // console.log(gamename.__proto__)
+
+// // console.log(gamename.length)
+// // console.log(gamename.toUpperCase());
+
+// // console.log(gamename.charAt(1));
+// // // console.log(gamename.charCodeAt(1));
+// // console.log(gamename.indexOf('u'));
+
+// // const newstr=gamename.substring(0,4)
+// // console.log(newstr)
+
+// const anotherstr=gamename.slice(0,4)
+// console.log(anotherstr)
+
+// const newstr2="  ashutosh   ";
+// console.log(newstr2.trim());
+
+// const url="https://ashutosh.com/ashutosh%20singh%20_01020"
+// url.replace("%20","-")//url.replace("%20", "-") returns a new string with the first occurrence of %20 replaced by -.
+// console.log(url.replace("%20","-"))
+// //If you need to replace all occurrences of %20 with -, use a regular expression with the global flag (g):
+
+// console.log(url.replace(/%20/g,'-'))
+
+// console.log(url.includes('ash'))
+// console.log(url.startsWith('https://ashutosh.com/ashutosh'))
+// console.log(url.endsWith('01020'))
+
+const newurl="https://ashutosh_singh.com/ashutosh/01010_2apnd"
+
+const newgame=new String('ashutosh-hc-com')
+console.log(newgame.split('-')) //so it  converts string based on the value provided
+
+const newvalue=new String('hey,hlw wassup, all good')
+console.log(newvalue.split(',')) //so it  converts string based on the value provided
+
+
+
+
